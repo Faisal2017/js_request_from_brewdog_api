efde42c58a1ade32e5b166e403fd9a04197ed42c
Faisal <faisal_a@live.co.uk>
2017-08-02 17:24:29 +0100

    request to api being made and returning json data

--- public/index.js
+++ public/index.js
@@ -1,7 +1,20 @@
 var app = function () {
-
-
-
+  var url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
 }
+
+var makeRequest = function(url, callback) {
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', callback);
+
+  request.open('GET', url);
+  request.send();
+}
+
+var requestComplete = function() {
+  console.log(this);
+}
+
+
 
 window.addEventListener('load', app);
