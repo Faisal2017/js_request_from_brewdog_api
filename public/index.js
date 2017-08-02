3a0a0e97c40cbc0060071b9026efc6e555372bcf
Faisal <faisal_a@live.co.uk>
2017-08-02 17:37:00 +0100

    list of beers being displayed in browser

--- public/index.js
+++ public/index.js
@@ -12,9 +12,20 @@ var makeRequest = function(url, callback) {
 }
 
 var requestComplete = function() {
-  console.log(this);
+  if (this.status != 200) return;
+  console.log(this)
+  var jsonString = this.responseText;
+  var beers = JSON.parse(jsonString);
+  populateList(beers)
 }
 
-
+var populateList = function(beers) {
+  var ul = document.querySelector('#beer-list');
+  beers.forEach(function(beer) {
+    var li = document.createElement('li')
+    li.innerText = beer.name;
+    ul.appendChild(li);
+  });
+}
 
 window.addEventListener('load', app);
